b2111c45ae88d5a90f6297b03daff88c40e138b3
Nikita <Niktos.V@tut.by>
2018-09-16 10:22:51 +0300

    add init id to client

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 // server.js
-const net = require('net');
-const port = 8124;
+const net = require('net'); // подключаю net
+const port = 8124;          // постоянная переменная содержащая номер порта
+let seed = 0;               // инициализирую сид
 
 const server = net.createServer((client) => {
     console.log('Client connected');
+
+    client.id = Date.now() + seed++; // добавление id клиенту
 
     client.setEncoding('utf8');
 
