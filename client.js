bd4dcfca7ba52c296b1b86065f3e39994c2915f6
Nikita <Niktos.V@tut.by>
2018-09-17 09:10:10 +0300

    add big changes in all files

--- client.js
+++ client.js
@@ -1,43 +1,53 @@
 // client.js
 const net = require('net');
-const fs = require('fs');   // добавление fs
+const fs = require('fs');
 const port = 8124;
+const string = 'QA';
+const bad = 'DEC';
+const good = 'ACK';
 
 const client = new net.Socket();
-
+let currentIndex = -1;
 client.setEncoding('utf8');
-let arrquery;
 
-client.connect(port, function() {
-    console.log('Connected');
-    fs.readFile('qa.json', (e,text) => {
-
-        if(e) {
-            console.log(e)
+let questions = [];
+client.connect({port: port, host: '127.0.0.1'}, () => {
+    fs.readFile("qa.json", (err, text) => {
+        if (!err) {
+            questions = JSON.parse(text);
+            client.write(string);
         }
-        else {
-            arrquery = sh(JSON.parse(text));
-            client.write('QA');
-        }
+        else console.error(err);
     });
 });
 
-client.on('data', function(data) {
-    console.log(data);
-    client.destroy();
+client.on('data', (data) => {
+    if (data === bad)
+        client.destroy();
+    if (data === good)
+        sendQuestion();
+    else {
+        let qst = questions[currentIndex];
+        let answer = qst.good;
+        console.log('\n' + qst.quest);
+        console.log('Answer:' + data);
+        console.log('Server:' + answer);
+        console.log('Result:' + (data === answer ? 'True': 'False'));
+        sendQuestion();
+    }
 });
 
-client.on('close', function() {
+client.on('close', function () {
     console.log('Connection closed');
 });
 
-function sh(array) {
-    let a;
-    var result = [];
-    while(array.length > 0){
-        a = Math.floor(Math.random() * array.length);
-        result.push(array[a]);
-        array.splice(a, 1);
+
+
+function sendQuestion() {
+    if (currentIndex < questions.length -1) {
+        let qst = questions[++currentIndex].quest;
+        client.write(qst);
     }
-    return result;
+    else
+        client.destroy();
 }
--- server.js
+++ server.js
@@ -1,33 +1,48 @@
 // server.js
-const net = require('net'); // подключаю net
-const port = 8124;          // постоянная переменная содержащая номер порта
-let seed = 0;               // инициализирую сид
+const net = require('net'); // подключаем net
+const fs = require('fs');   // подключаем fs
+const port = 8124;          // константа содержащая порт 8124
+const clientString = 'QA';
+const good = 'ACK';
+const bad = 'DEC';
+const logger = fs.createWriteStream('client_id.log');   // создание файла log
+
+let seed = 0;      // инициализируем сид
 
 const server = net.createServer((client) => {
     console.log('Client connected');
-
-    client.id = Date.now() + seed++; // добавление id клиенту
     client.setEncoding('utf8');
 
-    console.log('[' + formatDate() + ']: ' +'Client #' + client.id + ' connected\n');  // вывод инфы
-
-    client.on('data', (data) => {
-        if (data === 'QA') client.write('ACK');
-        else {
-            let answr = Math.floor(Math.random() * 2).toString(); // установка ответа, округление до целогочисла с попощью floor()
-            client.write(answr);
+    client.on('data', (data, err) =>
+    {
+        if (err) console.error(err);
+        else if (!err && data === clientString)
+        {
+            client.id = Date.now() + seed++;
+            writeLog('Client #' + client.id + ' connected\n');
+            client.write(data === clientString ? good : bad);
+        }
+        else if (!err && data !== clientString) {
+            writeLog('Client #' + client.id + ' has asked: ' + data + '\n');
+            let answer = generateAnswer();
+            writeLog('Server answered to Client #' + client.id + ': ' + answer + '\n');
+            client.write(answer);
         }
     });
-
-    client.on('end', () => {
-    console.log('Client disconnected');
+    client.on('end', () =>
+    {
+        logger.write('Client #'+ client.id+ ' disconnected');
+        console.log('Client disconnected')
     });
 });
-
-server.listen(port, () => {
-    console.log(`Server listening on localhost:${port}`);
-});
-
-function formateDate() {
-    return new Date.toISOString().replace(/T/, ' ').replace(/\..+/, '');
+function writeLog(data)
+{
+    logger.write(data);
 }
+function generateAnswer()
+{
+    return Math.random() > 0.5 ? '1' : '0';
+}
+server.listen(port, () => {
+    console.log(`Server listening on localhost: ${port}`);
+});
